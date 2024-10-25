d8e7c65a3254dbd6de4b51e7f02373dfcef25ed8
Oliver Walsh <owalsh@redhat.com>
2024-10-25 11:54:28 +0100

    Inherit top-level nodeselector in rabbitmq spec

--- pkg/openstack/rabbitmq.go
+++ pkg/openstack/rabbitmq.go
@@ -310,6 +310,14 @@ func reconcileRabbitMQ(
 		tlsCert = certSecret.Name
 	}
 
+	if spec.Override.StatefulSet == nil {
+		spec.Override.StatefulSet = &defaultStatefulSet
+	}
+
+	if spec.Override.StatefulSet.Spec.Template.Spec.NodeSelector == nil {
+		spec.Override.StatefulSet.Spec.Template.Spec.NodeSelector = instance.Spec.NodeSelector
+	}
+
 	op, err := controllerutil.CreateOrPatch(ctx, helper.GetClient(), rabbitmq, func() error {
 
 		rabbitmq.Spec.Image = *version.Status.ContainerImages.RabbitmqImage
@@ -360,11 +368,6 @@ func reconcileRabbitMQ(
 		if rabbitmq.Spec.Persistence.StorageClassName == nil {
 			Log.Info(fmt.Sprintf("Setting StorageClassName: " + instance.Spec.StorageClass))
 			rabbitmq.Spec.Persistence.StorageClassName = &instance.Spec.StorageClass
-		}
-
-		if rabbitmq.Spec.Override.StatefulSet == nil {
-			Log.Info("Setting StatefulSet")
-			rabbitmq.Spec.Override.StatefulSet = &defaultStatefulSet
 		}
 
 		if rabbitmq.Spec.Override.Service != nil &&
